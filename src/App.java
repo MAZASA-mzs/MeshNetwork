474f1a913434b714d616789b703b7e13d9cfd232
sln404 <gryukach.ilya@gmail.com>
2024-10-14 01:54:46 +0300

    Fixed some VS code issue

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("It's MAZASA time!!!");
-        System.out.println("Fuck Windows!!!");
+        System.out.println("It's SLN404 time!!!");
     }
 }
