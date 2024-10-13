8c042f879401241a5da9aa5cbd8a258793225b05
sln404 <gryukach.ilya@gmail.com>
2024-10-14 01:49:24 +0300

    Fuck offf, it's 2 am time and Ifuck with repos...

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("It's MAZASA time!!!");
+        System.out.println("Fuck Windows!!!");
     }
 }
