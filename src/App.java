eb3467d40ce47bee04652c80e0ad3ff86bee9773
sln404 <gryukach.ilya@gmail.com>
2024-11-14 15:55:27 +0300

    Second try

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("It's SLN404 time!!! Fixing fies.");
+        System.out.println("It's SLN404 time!!! Fixing fies. Try infty/2");
     }
 }
