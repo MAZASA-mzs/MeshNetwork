ab435f112f64fbdb2d4b5a2312b88a16fbe7da78
sln404 <gryukach.ilya@gmail.com>
2024-11-15 19:59:01 +0300

    Bag fixes after merge commit

--- src/org/controller/Controller.java
+++ src/org/controller/Controller.java
@@ -14,32 +14,24 @@ import org.model.ids.NodeID;
  */
 public class Controller {
 
-    private NodeStorage nodeStorage;
-    private ConnectionStorage connectionStorage;
-
-    public Controller(NodeStorage nodeStorage, ConnectionStorage connectionStorage) {
-        this.nodeStorage = nodeStorage;
-        this.connectionStorage = connectionStorage;
-    }
-
     public void update() {
         // Update nodes and connections
     }
 
     public void addNode(Node node) {
-        nodeStorage.add(node);
+        NodeStorage.add(node);
     }
 
     public void removeNode(NodeID nodeId) {
-        nodeStorage.remove(nodeId);
+        NodeStorage.remove(nodeId);
     }
 
     public void addConnection(Connection connection) {
-        connectionStorage.add(connection);
+        ConnectionStorage.add(connection);
     }
 
     public void removeConnection(ConnectionID connectionId) {
-        connectionStorage.remove(connectionId);
+        ConnectionStorage.remove(connectionId);
     }
 
     public String getViewData() {
